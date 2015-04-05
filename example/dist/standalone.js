8f009eb77bb119fe4d601e22a36e88226a2c2b64
Jed Watson <jed.watson@me.com>
2015-04-05 22:59:18 +1000

    New build, changes due to babel update

--- example/dist/standalone.js
+++ example/dist/standalone.js
@@ -1,16 +1,16 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.AutosizeInput = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
-"use strict";
+'use strict';
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
 var React = (typeof window !== "undefined" ? window.React : typeof global !== "undefined" ? global.React : null);
 
-var sizerStyle = { position: "absolute", visibility: "hidden", height: 0, width: 0, overflow: "scroll", whiteSpace: "nowrap" };
+var sizerStyle = { position: 'absolute', visibility: 'hidden', height: 0, width: 0, overflow: 'scroll', whiteSpace: 'nowrap' };
 
 var AutosizeInput = React.createClass({
 
-	displayName: "AutosizeInput",
+	displayName: 'AutosizeInput',
 
 	propTypes: {
 		value: React.PropTypes.any, // field value
@@ -61,7 +61,7 @@ var AutosizeInput = React.createClass({
 	},
 
 	updateInputWidth: function updateInputWidth() {
-		if (!this.isMounted() || typeof this.refs.sizer.getDOMNode().scrollWidth === "undefined") {
+		if (!this.isMounted() || typeof this.refs.sizer.getDOMNode().scrollWidth === 'undefined') {
 			return;
 		}
 		var newInputWidth;
@@ -94,25 +94,25 @@ var AutosizeInput = React.createClass({
 
 	render: function render() {
 
-		var nbspValue = (this.props.value || "").replace(/ /g, "&nbsp;");
+		var nbspValue = (this.props.value || '').replace(/ /g, '&nbsp;');
 
 		var wrapperStyle = this.props.style || {};
-		wrapperStyle.display = "inline-block";
+		wrapperStyle.display = 'inline-block';
 
 		var inputStyle = this.props.inputStyle || {};
 		inputStyle.width = this.state.inputWidth;
 
 		var placeholder = this.props.placeholder ? React.createElement(
-			"div",
-			{ ref: "placeholderSizer", style: sizerStyle },
+			'div',
+			{ ref: 'placeholderSizer', style: sizerStyle },
 			this.props.placeholder
 		) : null;
 
 		return React.createElement(
-			"div",
+			'div',
 			{ className: this.props.className, style: wrapperStyle },
-			React.createElement("input", _extends({}, this.props, { ref: "input", className: this.props.inputClassName, style: inputStyle })),
-			React.createElement("div", { ref: "sizer", style: sizerStyle, dangerouslySetInnerHTML: { __html: nbspValue } }),
+			React.createElement('input', _extends({}, this.props, { ref: 'input', className: this.props.inputClassName, style: inputStyle })),
+			React.createElement('div', { ref: 'sizer', style: sizerStyle, dangerouslySetInnerHTML: { __html: nbspValue } }),
 			placeholder
 		);
 	}
