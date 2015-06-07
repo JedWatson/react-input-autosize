7d662f6381e12c1f460018bf6c2d39c1615307ea
Jed Watson <jed.watson@me.com>
2015-06-07 23:19:55 +1000

    Updating build

--- dist/react-input-autosize.js
+++ dist/react-input-autosize.js
@@ -1,15 +1,13 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.AutosizeInput = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (global){
 'use strict';
 
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var React = (typeof window !== "undefined" ? window.React : typeof global !== "undefined" ? global.React : null);
+var React = (window.React);
 
 var sizerStyle = { position: 'absolute', visibility: 'hidden', height: 0, width: 0, overflow: 'scroll', whiteSpace: 'nowrap' };
 
 var AutosizeInput = React.createClass({
-
 	displayName: 'AutosizeInput',
 
 	propTypes: {
@@ -23,28 +21,23 @@ var AutosizeInput = React.createClass({
 		inputStyle: React.PropTypes.object, // css styles for the input element
 		inputClassName: React.PropTypes.string // className for the input element
 	},
-
 	getDefaultProps: function getDefaultProps() {
 		return {
 			minWidth: 1
 		};
 	},
-
 	getInitialState: function getInitialState() {
 		return {
 			inputWidth: this.props.minWidth
 		};
 	},
-
 	componentDidMount: function componentDidMount() {
 		this.copyInputStyles();
 		this.updateInputWidth();
 	},
-
 	componentDidUpdate: function componentDidUpdate() {
 		this.updateInputWidth();
 	},
-
 	copyInputStyles: function copyInputStyles() {
 		if (!this.isMounted() || !window.getComputedStyle) {
 			return;
@@ -59,7 +52,6 @@ var AutosizeInput = React.createClass({
 			placeholderNode.style.fontFamily = inputStyle.fontFamily;
 		}
 	},
-
 	updateInputWidth: function updateInputWidth() {
 		if (!this.isMounted() || typeof this.refs.sizer.getDOMNode().scrollWidth === 'undefined') {
 			return;
@@ -79,48 +71,37 @@ var AutosizeInput = React.createClass({
 			});
 		}
 	},
-
 	getInput: function getInput() {
 		return this.refs.input;
 	},
-
 	focus: function focus() {
 		this.refs.input.getDOMNode().focus();
 	},
-
 	select: function select() {
 		this.refs.input.getDOMNode().select();
 	},
-
 	render: function render() {
-
-		var nbspValue = (this.props.value || '').replace(/ /g, '&nbsp;');
-
+		var escapedValue = (this.props.value || '').replace(/ /g, '&nbsp;').replace(/\</g, '&lt;').replace(/\>/g, '&gt;');
 		var wrapperStyle = this.props.style || {};
 		wrapperStyle.display = 'inline-block';
-
 		var inputStyle = this.props.inputStyle || {};
 		inputStyle.width = this.state.inputWidth;
-
 		var placeholder = this.props.placeholder ? React.createElement(
 			'div',
 			{ ref: 'placeholderSizer', style: sizerStyle },
 			this.props.placeholder
 		) : null;
-
 		return React.createElement(
 			'div',
 			{ className: this.props.className, style: wrapperStyle },
 			React.createElement('input', _extends({}, this.props, { ref: 'input', className: this.props.inputClassName, style: inputStyle })),
-			React.createElement('div', { ref: 'sizer', style: sizerStyle, dangerouslySetInnerHTML: { __html: nbspValue } }),
+			React.createElement('div', { ref: 'sizer', style: sizerStyle, dangerouslySetInnerHTML: { __html: escapedValue } }),
 			placeholder
 		);
 	}
-
 });
 
 module.exports = AutosizeInput;
 
-}).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}]},{},[1])(1)
 });
