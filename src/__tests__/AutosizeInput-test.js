f46ac6f8f088ec01aead53985a67c7228eedcff4
Davi Ferreira <contato@daviferreira.com>
2015-09-27 13:52:30 +0200

    Get ready for react 0.14

--- src/__tests__/AutosizeInput-test.js
+++ src/__tests__/AutosizeInput-test.js
@@ -3,9 +3,9 @@
 
 jest.dontMock('../AutosizeInput');
 
-var React = require('react/addons');
+var React = require('react');
 var AutosizeInput = require('../AutosizeInput');
-var TestUtils = React.addons.TestUtils;
+var TestUtils = require('react-addons-test-utils');
 
 describe('AutosizeInput test', function() {
 
