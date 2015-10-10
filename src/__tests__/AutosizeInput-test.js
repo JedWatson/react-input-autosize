8c4f61cd006333429114fc3793ea865fa5c0a148
Jed Watson <jed.watson@me.com>
2015-10-10 23:29:24 +1100

    Updating tests for React 0.14

--- src/__tests__/AutosizeInput-test.js
+++ src/__tests__/AutosizeInput-test.js
@@ -3,9 +3,9 @@
 
 jest.dontMock('../AutosizeInput');
 
-var React = require('react/addons');
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
 var AutosizeInput = require('../AutosizeInput');
-var TestUtils = React.addons.TestUtils;
 
 describe('AutosizeInput test', function() {
 
@@ -21,7 +21,7 @@ describe('AutosizeInput test', function() {
 	var inputElement = TestUtils.findRenderedDOMComponentWithTag(instance, 'input');
 
 	it('should assign the given inputClassName', function() {
-		expect(inputElement.getDOMNode().className).toEqual('testClass');
+		expect(inputElement.className).toEqual('testClass');
 	});
 
 });
