a48d546c3c94b6d8052e9aeda31ba503322b2597
Jed Watson <jed.watson@me.com>
2015-10-11 23:40:38 +1100

    Adding explicit dependencies (ref #18)

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,14 @@
-var gulp = require('gulp'),
-    initGulpTasks = require('react-component-gulp-tasks');
+var gulp = require('gulp');
+var initGulpTasks = require('react-component-gulp-tasks');
 
 var taskConfig = {
 
 	component: {
-		name: 'AutosizeInput'
+		name: 'AutosizeInput',
+		dependencies: [
+			'react',
+			'react-dom'
+		]
 	},
 
 	example: {
