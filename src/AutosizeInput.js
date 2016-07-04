61dd3637fdaedc9ef68727878d0a7e924b6c5142
Caedmon Irias <grillo321@gmail.com>
2016-07-04 09:34:09 -0700

    more consistent approach to inputProps creation

--- src/AutosizeInput.js
+++ src/AutosizeInput.js
@@ -96,14 +96,14 @@ const AutosizeInput = React.createClass({
 		const inputStyle = Object.assign({}, this.props.inputStyle);
 		inputStyle.width = this.state.inputWidth + 'px';
 		inputStyle.boxSizing = 'content-box';
-		const { defaultValue, onChange, value } = this.props;
-		const inputProps = { defaultValue, onChange, value, placeholder: this.props.placeholder };
-		const placeholder = this.props.placeholder ? <div ref="placeholderSizer" style={sizerStyle}>{this.props.placeholder}</div> : null;
+		const { defaultValue, onChange, placeholder, value } = this.props;
+		const inputProps = { defaultValue, onChange, value, placeholder };
+		const placeholderElement = placeholder ? <div ref="placeholderSizer" style={sizerStyle}>{placeholder}</div> : null;
 		return (
 			<div className={this.props.className} style={wrapperStyle}>
 				<input {...inputProps} ref="input" className={this.props.inputClassName} style={inputStyle} />
 				<div ref="sizer" style={sizerStyle}>{sizerValue}</div>
-				{placeholder}
+				{placeholderElement}
 			</div>
 		);
 	},
