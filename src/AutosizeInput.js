JedWatson/react-input-autosize

commit 5da2322b840218781fa42d070efbef2473c8c246

Replace use of this.isMounted()

src/AutosizeInput.js
@@ -40,6 +40,7 @@ const AutosizeInput = createClass({
 		};
 	},
 	componentDidMount () {
+		this.mounted = true;
 		this.copyInputStyles();
 		this.updateInputWidth();
 	},
@@ -51,6 +52,9 @@ const AutosizeInput = createClass({
 		}
 		this.updateInputWidth();
 	},
+	componentWillUnmount () {
+		this.mounted = false;
+	},
 	inputRef (el) {
 		this.input = el;
 	},
@@ -61,7 +65,7 @@ const AutosizeInput = createClass({
 		this.sizer = el;
 	},
 	copyInputStyles () {
-		if (!this.isMounted() || !window.getComputedStyle) {
+		if (this.mounted || !window.getComputedStyle) {
 			return;
 		}
 		const inputStyle = this.input && window.getComputedStyle(this.input);
@@ -86,7 +90,7 @@ const AutosizeInput = createClass({
 		}
 	},
 	updateInputWidth () {
-		if (!this.isMounted() || !this.sizer || typeof this.sizer.scrollWidth === 'undefined') {
+		if (!this.mounted || !this.sizer || typeof this.sizer.scrollWidth === 'undefined') {
 			return;
 		}
 		let newInputWidth;

commit dcf7d221eb8f34dd45c67495b00816be2c29ea00

Fix copyInputStyles() never running.
This was broken in 5da2322b840218781fa42d070efbef2473c8c246.

src/AutosizeInput.js
@@ -40,9 +40,9 @@ const AutosizeInput = createClass({
 		};
 	},
 	componentDidMount () {
-		this.mounted = true;
 		this.copyInputStyles();
 		this.updateInputWidth();
+		this.mounted = true;
 	},
 	componentDidUpdate (prevProps, prevState) {
 		if (prevState.inputWidth !== this.state.inputWidth) {