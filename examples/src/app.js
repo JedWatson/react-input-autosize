1ced919a26e5f55e0ce85ca205c4b61af744322d
Michal Landsman <landsman@insuit.cz>
2021-12-03 16:44:53 +0100

    reproduce error - decimals not working

--- examples/src/app.js
+++ examples/src/app.js
@@ -30,6 +30,13 @@ class App extends Component {
 					autoFocus
 					value={this.state.value1}
 					onChange={this.updateInputValue.bind(this, 'value1')}
+					placeholder={'0'}
+					type={'number'}
+					step={'0.10'}
+					inputMode={'decimal'}
+					min={0.01}
+					max={1000000}
+					pattern={'[0-9]*'}
 				/>
 				<h3>Styled example with default value:</h3>
 				<AutosizeInput
